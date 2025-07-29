76dea75ff71fa2a6d3a90835605edf9cb88bab84
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2025-07-30 00:18:05 +0100

    update init

--- display/configs/__init__.py
+++ display/configs/__init__.py
@@ -1,4 +1,2 @@
-from .display_configs import display2_config, inky_config, waveshare_3in7_eink_config
+from .display_configs import *
 from .bases import *
-
-__all__ = ['display2_config', 'inky_config', 'waveshare_3in7_eink_config']
