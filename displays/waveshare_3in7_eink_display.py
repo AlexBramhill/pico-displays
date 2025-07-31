3d5500d95701e48815722811ae6314442dd56096
Alex Bramhill <47955140+AlexBramhill@users.noreply.github.com>
2025-07-31 01:13:22 +0100

    update waveshare

--- displays/waveshare_3in7_eink_display.py
+++ displays/waveshare_3in7_eink_display.py
@@ -1,11 +1,13 @@
 from ..abstract.display_base import DisplayBase
 from ..enums import COLOUR_PROFILE, IMAGE_TYPE
-from ..drivers.waveshare_pico_epaper_3in7 import EPD_3in7
 
 
 class Waveshare3In7EinkDisplay(DisplayBase):
 
     def __init__(self, max_update_speed_in_ms=0, hard_refresh_interval=10):
+        # Import the driver only when the class is initialized
+        from ..drivers.waveshare_pico_epaper_3in7 import EPD_3in7
+
         super().__init__(max_update_speed_in_ms, hard_refresh_interval)
         self._instance = EPD_3in7()
 
@@ -15,31 +17,26 @@ class Waveshare3In7EinkDisplay(DisplayBase):
         }
 
     def _hard_refresh(self):
-        """Hard refresh - clear display first to remove ghosting."""
-        # Clear based on the last used color profile
-        # We'll need to track the last color profile used
-        if hasattr(self, '_last_color_profile'):
-            if self._last_color_profile == COLOUR_PROFILE.ONE_BIT:
-                self._clear_1bit()
-            elif self._last_color_profile == COLOUR_PROFILE.TWO_BIT:
-                self._clear_2bit()
+        if not hasattr(self, '_last_color_profile'):
+            return
 
-    def _clear_1bit(self):
+        if self._last_color_profile == COLOUR_PROFILE.ONE_BIT:
+            self._hard_refresh_1bit()
+        elif self._last_color_profile == COLOUR_PROFILE.TWO_BIT:
+            self._hard_refresh_2bit()
+
+    def _hard_refresh_1bit(self):
         self._instance.EPD_3IN7_1Gray_Clear()
 
-    def _clear_2bit(self):
+    def _hard_refresh_2bit(self):
         self._instance.EPD_3IN7_4Gray_Clear()
 
     def _draw_bmp_1bit(self, img_data):
         self._last_color_profile = COLOUR_PROFILE.ONE_BIT
-        if hasattr(self._instance, 'EPD_3IN7_1Gray_Display'):
-            self._instance.EPD_3IN7_1Gray_Display(img_data)
-            return True
-        return False
+        self._instance.EPD_3IN7_1Gray_Display(img_data)
+        return True
 
     def _draw_bmp_2bit(self, img_data):
         self._last_color_profile = COLOUR_PROFILE.TWO_BIT
-        if hasattr(self._instance, 'EPD_3IN7_4Gray_Display'):
-            self._instance.EPD_3IN7_4Gray_Display(img_data)
-            return True
-        return False
+        self._instance.EPD_3IN7_4Gray_Display(img_data)
+        return True
